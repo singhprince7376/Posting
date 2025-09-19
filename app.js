362a505f0e0eac76626ab81b4a462cb0de61f811
singhprince7376 <145587336+singhprince7376@users.noreply.github.com>
2025-09-19 08:19:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
